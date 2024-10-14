ad5aa217ad21173674093e2dbec80696f8cd9672
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-10-14 01:27:38 -0300

    First Commit

---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+export interface Project {
+	id: string;
+	title: string;
+	description: string;
+	technologies: string;
+	url: string;
+	image: string;
+	repo: string;
+	date: string;
+	status: string;
+	category: string;
+}
