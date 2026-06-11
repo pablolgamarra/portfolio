66b9977a30268c44422a646c74c412aa99805458
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2026-06-11 17:18:35 -0300

    feat: restructure portfolio site with new Navbar component and global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,3 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({
-  site: "https://astro-portfolio-uzair.vercel.app",
-  integrations: [
-    tailwind(),
-    sitemap({
-      changefreq: "weekly",
-      priority: 0.7,
-      lastmod: new Date(),
-      entryLimit: 10000,
-    }),
-    robotsTxt(),
-  ],
-});
+export default defineConfig({});
